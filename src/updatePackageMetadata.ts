33adf387518dcf10f3780639b320a6b53d287590
Justin Neff <justin@gatewayapps.com>
2019-04-26 09:23:56 -0500

    Support retrying when updating packages metadata

--- src/updatePackageMetadata.ts
+++ src/updatePackageMetadata.ts
@@ -1,20 +1,48 @@
+import wait from 'waait'
 import { sendHubRequest } from './request'
 import { IPackageMetadata } from './types/IPackageMetadata'
 import { IPackageMetadataRequestBody } from './types/IPackageMetadataRequestBody'
 import { signDataWithSecret } from './signDataWithSecret'
 
+const DELAY_FACTOR = 2
+const DELAY_MULTIPLIER = 1000
+
 export async function updatePackageMetadata(
   metadata: IPackageMetadata,
-  secret: string
+  secret: string,
+  maxAttempts: number = 5
 ): Promise<boolean> {
-  const endpoint = '/api/package/metadata'
   const body: IPackageMetadataRequestBody = {
     data: metadata,
     signature: signDataWithSecret(metadata, secret)
   }
-  const result = await sendHubRequest(endpoint, undefined, body)
-  if (!result.success) {
-    throw new Error(result.message || 'Unexpected response sending package metadata to hub')
+  return await attemptUpdatePackageMetadata(body, maxAttempts)
+}
+
+async function attemptUpdatePackageMetadata(
+  body: IPackageMetadataRequestBody,
+  maxAttempts: number,
+  attempt: number = 1
+): Promise<boolean> {
+  let result: any
+  try {
+    const endpoint = '/api/package/metadata'
+    result = await sendHubRequest(endpoint, undefined, body)
+  } catch (err) {
+    result = {
+      success: false,
+      message: err.message
+    }
   }
-  return true
+
+  if (result && result.success) {
+    return true
+  }
+
+  if (attempt < maxAttempts) {
+    const delay = Math.pow(DELAY_FACTOR, attempt) * DELAY_MULTIPLIER
+    await wait(delay)
+    return attemptUpdatePackageMetadata(body, maxAttempts, attempt + 1)
+  }
+  throw new Error(result.message || 'Unexpected response sending package metadata to hub')
 }
